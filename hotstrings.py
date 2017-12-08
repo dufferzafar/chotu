d504af3bb1b1a84f419fd9fb000552d9d5d8a0ec
Shadab Zafar <dufferzafar0@gmail.com>
2017-12-09 00:55:19 +0530

    Add some hotstrings: /name /dz /blog /gh

---
+++ hotstrings.py
@@ -0,0 +1,35 @@
+from datetime import datetime as dt
+
+from PyQt5 import QtCore as QtC
+
+import lib_hotstrings as lhs
+Xctx = lhs.get_Xctx()
+
+
+# Keys are replaced with values as you type them
+hotstrings = {
+    # Personal Information
+    "/name": "Shadab Zafar",
+
+    "/dz": "dufferzafar",
+    "/blog": "http://dufferzafar.github.io/",
+    "/git": "http://github.com/dufferzafar/",
+
+    # Misc
+    "/hah": "hahahahahaha",
+    "/lmgtfy": "http://lmgtfy.com/?q=",
+
+    # Functions
+    "/date": dt.now().strftime("%d/%m/%Y"),
+    "/time": dt.now().strftime("%I:%M %p"),
+}
+
+
+class Hotstrings(QtC.QThread):
+
+    def run(self):
+        lhs.watch(hotstrings, *Xctx)
+
+    @classmethod
+    def cleanup(cls):
+        lhs.cleanup(*Xctx)
